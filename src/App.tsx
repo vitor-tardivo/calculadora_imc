318eb01666334109b0e16960b12cffdf93d3ab50
vitor-tardivo <vitorgt.ovidart@hotmail.com>
2025-04-01 22:15:15 -0300

    Inicio da edição do aplicativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Card, Box, Typography } from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'flex-start'}
+      width={'100vw'}
+      height={'100vh'}
+    >
+      <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '100%',
+        marginTop: '144px',
+        marginX: '16px',
+      }}
+      >
+
+        <Box
+          marginY={`16px`}
+        >
+          <Typography variant='h5' fontWeight={'700'} >
+            Calculadora IMC
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography>
+            entradas
+          </Typography>
+        </Box>
+
+        <Box>
+          <Typography>
+            resultado
+          </Typography>
+        </Box>
+
+      </Card>
+    </Box>
   )
 }
-
-export default App
